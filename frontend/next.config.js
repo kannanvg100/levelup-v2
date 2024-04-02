dc9746747cd5924a57a2e1c4fce7962e889c485d
kannanvg100 <kannanx200@gmail.com>
2024-04-02 11:19:57 +0530

    config updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,42 +1,44 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: 'http://localhost:5000/api/:path*',
-			},
-		]
-	},
-	images: {
-		// domains: ['levelup.s3.ap-south-1.amazonaws.com', 'lh3.googleusercontent.com', 'image.mux.com'],
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'levelup.s3.ap-south-1.amazonaws.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'lh3.googleusercontent.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'image.mux.com',
-				port: '',
-				pathname: '/**',
-			},
-		],
-	},
-	webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-		config.module.rules.push({
-			test: /\.mjs$/,
-			include: /node_modules/,
-			type: 'javascript/auto',
-		})
-		return config
-	},
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+            },
+        ]
+    },
+    images: {
+        // domains: ['levelup.s3.ap-south-1.amazonaws.com', 'lh3.googleusercontent.com', 'image.mux.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'levelup.s3.ap-south-1.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'image.mux.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+        config.module.rules.push({
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: 'javascript/auto',
+        })
+        return config
+    },
+    reactStrictMode: false
 }
