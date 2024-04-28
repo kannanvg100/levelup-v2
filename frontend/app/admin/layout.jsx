c124f0df6882dd144d6d7c5efd790ddac7e7f81f
kannanvg100 <kannanx200@gmail.com>
2024-04-28 15:12:15 +0530

    bug fixes

--- frontend/app/admin/layout.jsx
+++ frontend/app/admin/layout.jsx
@@ -1,5 +1,27 @@
+'use client'
 import React from 'react'
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+
+const queryClient = new QueryClient({
+	queryCache: new QueryCache({
+		onError: (error) => {
+			if (error?.response?.status === 401) {
+				router.push('/teacher/login')
+			} else if (error?.response?.status === 500) {
+                toast.error(error?.response?.data?.message || error?.response?.message || 'Something went wrong')
+			}
+		},
+	}),
+	mutationCache: new MutationCache({
+		onError: (error) => {
+			if (error?.response?.status === 401) {
+				router.push('/teacher/login')
+			}
+		},
+	}),
+})
 
 export default function layout({ children }) {
-	return <div>{children}</div>
+	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
